eef88686931bae0799d4624b03d2080571b4a6fb
Kentaro Oya <k.oya.cingroup2012@gmail.com>
2025-05-05 22:27:24 +0900

    クラスの実装

---
+++ src/basicSyntax/Main.java
@@ -0,0 +1,15 @@
+package basicSyntax;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		GeekIntroduction geekboy = new GeekIntroduction();
+		
+		geekboy.greeting(18, 181.3);
+		
+		geekboy.specialSkill(10,100);
+
+	}
+
+}
